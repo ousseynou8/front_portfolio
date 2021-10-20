33d82a9768c3b52770f8085e18a1e53c7e45c0f3
ousseynou8 <ousseynoufofana15@gmail.com>
2021-10-20 22:05:24 +0200

    20/10

--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 import stage  from "./img/stage.png";
+import librairie  from "./img/librairie.png";
 import img  from "./img/a.jpeg";
 
 export const portfolio = [
@@ -10,14 +11,14 @@ export const portfolio = [
   },
   {
     id: 2,
-    img: img,
-    link: "http://fofdesign.fr",
+    img: librairie,
+    link: "https://github.com/ousseynou8/librairie",
     technology:"javascript"
   },
   {
     id: 3,
-    img: img,
-    link: "http://fofdesign.fr",
+    img: librairie,
+    link: "https://librairie-zeta.vercel.app/",
     technology:"html"
   },
   {
